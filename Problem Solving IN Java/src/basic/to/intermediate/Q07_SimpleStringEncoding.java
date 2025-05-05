6cb1d5aa1ac50a8b31891f0f18b3b60f334210b8
SebastinAF <asntheking098@gmail.com>
2025-05-06 00:02:45 +0530

    Added Q07_SimpleStringEncoding.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q07_SimpleStringEncoding.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q07_SimpleStringEncoding.java
@@ -1,5 +1,28 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
+class SimpleStringEncoding{
+
+	public void StringEncoding(String str){
+
+		char[] ch = new char[5];
+
+		for(int k = 0; k < str.length(); k++){
+			ch[k] = str.charAt(k);
+		}
+
+		System.out.print("Encoded Word: ");
+
+		for (int i = 0; i <= 4; i++) {
+			char ch1 = ch[i];
+			int dv = ch1 - 1;
+			char ch2 = (char) dv;
+			System.out.print(ch2);
+		}
+	}
+}
+
 public class Q07_SimpleStringEncoding {
 	
 	/**
@@ -20,4 +43,14 @@ public class Q07_SimpleStringEncoding {
 	Encoded Word: vghsd
 	 */
 
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter a Five letter word: ");
+		String str = scanner.nextLine();
+
+		SimpleStringEncoding SSE = new SimpleStringEncoding();
+		SSE.StringEncoding(str);
+	}
 }

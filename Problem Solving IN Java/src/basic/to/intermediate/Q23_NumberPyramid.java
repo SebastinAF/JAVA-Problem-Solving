9895eca1251714b2f0a77277a0d01def93fb0ba2
SebastinAF <asntheking098@gmail.com>
2025-05-06 22:56:07 +0530

    Added Q23_NumberPyramid.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q23_NumberPyramid.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q23_NumberPyramid.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q23_NumberPyramid {
 	
 	/**This program prints a numeric pyramid pattern where numbers 
@@ -19,7 +21,7 @@ Input:
 Enter Number of Lines: 5
 
 Output:
-   1
+    1
    232
   34543
  4567654
@@ -29,4 +31,35 @@ Output:
 	 
 	 */
 
+
+	public static void main(String ags[]) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting number of lines to be printed
+		System.out.print("Enter Number of Lines: ");
+		int n = scanner.nextInt();
+
+		//using for loop printing the n number of rows
+		for (int i = 1; i <= n; i++){
+			// Print spaces to center the pyramid
+			for (int s = 1; s <= n - i; s++) {
+				System.out.print(" ");
+			}
+
+			// Print increasing numbers starting from i
+			int num = i;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(num++);
+			}
+
+			// Print decreasing numbers starting from num - 2
+			num = num - 2;
+			for (int j = 1; j < i; j++) {
+				System.out.print(num--);
+			}
+			System.out.println();
+		}
+
+	}
 }

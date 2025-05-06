16532c31eb4f3ba6ca3cf65f28d8c30b2ac523be
SebastinAF <asntheking098@gmail.com>
2025-05-06 18:19:38 +0530

    Added Q15_HeatAnalysis.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q15_HeatAnalysis.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q15_HeatAnalysis.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q15_HeatAnalysis {
 	
 	/**
@@ -25,5 +27,30 @@ public class Q15_HeatAnalysis {
 
 
 	 */
+
+	public static void main(String ags[]){
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting Heat code value from user rang from 1 to 3
+		System.out.print("Enter Heat code (1-3):");
+		int flagValue = scanner.nextInt();
+
+		//using switch case we are passing the flag value and printing the output by case values
+		switch (flagValue){
+			case 1:
+				System.out.println("Flag 1 is HOT");
+				break;
+			case 2:
+				System.out.println("Flag 2 is LUKE WARM");
+				break;
+			case 3:
+				System.out.println("Flag 3 is COLD");
+				break;
+			default:
+				System.out.println("OUT OF RANGE");
+		}
+
+	}
 
 }

97d5a86a9333e06ca48fa0997c16f383076125ab
SebastinAF <asntheking098@gmail.com>
2025-05-06 18:32:37 +0530

    Added Q16_ColorAnalysis.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q16_ColorAnalysis.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q16_ColorAnalysis.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q16_ColorAnalysis {
 	
 	/**
@@ -23,5 +25,30 @@ public class Q16_ColorAnalysis {
 		Color Code g is GREEN
 
 	 */
+
+	public static void main(String ags[]){
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting Color code value from user using char Data type
+		System.out.print("Enter Color code character: ");
+		char ColorCode = scanner.nextLine().toUpperCase().charAt(0);
+
+		//using switch case we are passing the ColoCode and printing the output by case values
+		switch (ColorCode){
+			case 'R':
+				System.out.println("Color Code r is RED");
+				break;
+			case 'G':
+				System.out.println("Color Code g is GREEN");
+				break;
+			case 'B':
+				System.out.println("Color Code b is BLUE");
+				break;
+			default:
+				System.out.println("Color Code " + ColorCode + " is BLACK");
+		}
+
+	}
 
 }

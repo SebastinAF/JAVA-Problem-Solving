e611eb5ebc4d144f7583289a3e59d46ec20f1067
SebastinAF <asntheking098@gmail.com>
2025-05-06 21:31:07 +0530

    Added Q20_FibonacciSeries.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q20_FibonacciSeries.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q20_FibonacciSeries.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q20_FibonacciSeries {
 	/**
 	 * This program calculates and displays the first n numbers in the 
@@ -21,4 +23,28 @@ public class Q20_FibonacciSeries {
 	1 1 2 3 5 8 13 21 34 55
 
 	 */
+
+	public static void main(String ags[]) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting number of terms from user using ind type
+		System.out.print("How many Fibonacci terms? : ");
+		int terms = scanner.nextInt();
+
+		//creating array to store the calculated value of Fibonacci
+		int[] numberOfTerms = new int[terms];
+		numberOfTerms[0] = 1;
+		numberOfTerms[1] = 1;
+
+		//using for loop add the values in previous indexed stored in array and store calculated value in next array index
+		for (int i = 2; i < terms; i++){
+			numberOfTerms[i] = numberOfTerms[i-1] + numberOfTerms[i-2];
+		}
+
+		//printing the values using foreach loop
+		for (int a : numberOfTerms){
+			System.out.print(a + " ");
+		}
+	}
 }

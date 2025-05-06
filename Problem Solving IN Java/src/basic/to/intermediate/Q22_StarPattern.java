5fbcbfd6ddd93c9da03822e08966d0d151dc118a
SebastinAF <asntheking098@gmail.com>
2025-05-06 22:47:24 +0530

    Added Q22_StarPattern.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q22_StarPattern.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q22_StarPattern.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q22_StarPattern {
 	
 	/**This program prints a right-aligned triangle star pattern using 
@@ -17,7 +19,7 @@ public class Q22_StarPattern {
 	Input:
 	How many Lines? 5
 	Output:
-   *
+    *
    **
   ***
  ****
@@ -25,4 +27,25 @@ public class Q22_StarPattern {
 
 	 */
 
+
+	public static void main(String ags[]) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting number of lines to be printed
+		System.out.print("How many Lines? : ");
+		int n = scanner.nextInt();
+
+		//using for loop and inner for loop we are printing n-1 spaces and number of *
+		for (int i = 1; i <= n; i++){
+			for (int j = 0; j < n-i; j++) {
+				System.out.print(" ");
+				}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
 }

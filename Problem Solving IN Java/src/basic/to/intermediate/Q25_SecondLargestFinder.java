33ba6841c554d96e1b6ae0c7af50b5ef86946337
SebastinAF <asntheking098@gmail.com>
2025-05-06 23:32:42 +0530

    Added Q25_SecondLargestFinder.java

--- Problem Solving IN Java/src/basic/to/intermediate/Q25_SecondLargestFinder.java
+++ Problem Solving IN Java/src/basic/to/intermediate/Q25_SecondLargestFinder.java
@@ -1,5 +1,7 @@
 package basic.to.intermediate;
 
+import java.util.Scanner;
+
 public class Q25_SecondLargestFinder {
 	
 	/**
@@ -33,4 +35,39 @@ Second largest number is: 22
 
 	 */
 
+	public static void main(String ags[]) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//getting the size of the array from user
+		System.out.print("Enter how many numbers: ");
+		int n = scanner.nextInt();
+
+		//creating array to store the n values and initialization of largest as 0 and SecondLargest 0
+		int[] numbers = new int[n];
+		int largest = 0, SecondLargest = 0;
+
+		//using for loop getting input from user and stored it to the array
+		for (int i = 0; i < numbers.length; i++){
+			numbers[i] = scanner.nextInt();
+		}
+
+		//using this for loop we are finding the first largest value in the array
+		for (int i = 0; i < numbers.length; i++){
+			if(numbers[i] > largest){
+				largest = numbers[i];
+			}
+		}
+
+		//using this for loop we are finding the first Second-largest value in the array by skipping the
+		// first largest value (Continue)
+		for (int i = 0; i < numbers.length; i++){
+			if(numbers[i] == largest ){
+				 continue;
+			} else if (numbers[i] > SecondLargest) {
+				SecondLargest = numbers[i];
+			}
+		}
+		System.out.println("Second largest number is: " + SecondLargest);
+	}
 }

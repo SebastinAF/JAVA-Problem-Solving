516ee695414915db5df8d5a8d7fecbf7f33c20ed
SebastinAF <asntheking098@gmail.com>
2026-03-13 08:00:23 +0530

    first commit

---
+++ src/Arrays/ConstructTransformedArrayMain.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+
+class Solution14 {
+    public int[] constructTransformedArray(int[] nums) {
+        int[] result = new int[nums.length];
+
+        return result;
+    }
+}
+
+public class ConstructTransformedArrayMain {
+
+    public static void main(String[] args) {
+
+        Solution14 S14 = new Solution14();
+
+        int[] answer = S14.constructTransformedArray(new int[]{3,-2,1,1});
+        System.out.println(Arrays.toString(answer));
+    }
+}

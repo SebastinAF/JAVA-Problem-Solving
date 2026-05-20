e8ad27fc82001328a9e56a302422382e095ab2c0
SebastinAF <asntheking098@gmail.com>
2026-05-20 09:12:41 +0530

    Sub Array Sum Solution Added

---
+++ src/PrefixSum/SubArraySumMain.java
@@ -0,0 +1,35 @@
+package PrefixSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution02 {
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
+
+public class SubArraySumMain {
+
+    public static void main(String[] args) {
+        Solution02 S02 = new Solution02();
+
+        System.out.println(S02.subarraySum(new int[]{0, 0, 0}, 0));
+    }
+}

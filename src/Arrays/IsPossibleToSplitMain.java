6c20c8dfe8c37f0f98d7e9dd06b575a92936a417
SebastinAF <asntheking098@gmail.com>
2026-06-07 14:39:37 +0530

    Is Possible To Split Solution Added

--- src/Arrays/IsPossibleToSplitMain.java
+++ src/Arrays/IsPossibleToSplitMain.java
@@ -1,4 +1,27 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution31 {
+    public boolean isPossibleToSplit(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int v : nums) {
+            map.put(nums[v], map.getOrDefault(nums[v], 0) + 1 );
+
+            if (map.get(nums[v]) > 2) return false;
+        }
+
+        return true;
+    }
+}
+
 public class IsPossibleToSplitMain {
+
+    public static void main(String[] args) {
+        Solution31 S31 = new Solution31();
+
+        System.out.println(S31.isPossibleToSplit(new int[]{1,1,2,2,3,4}));
+    }
 }

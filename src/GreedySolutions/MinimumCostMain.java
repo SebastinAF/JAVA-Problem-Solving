4c3f9090ca7f0894d74553f13e1c7ec022aa6c08
SebastinAF <asntheking098@gmail.com>
2026-06-27 13:40:09 +0530

    Minimum Cost Solution Added

--- src/GreedySolutions/MinimumCostMain.java
+++ src/GreedySolutions/MinimumCostMain.java
@@ -1,4 +1,32 @@
 package GreedySolutions;
 
+import java.util.Arrays;
+
+class Solution03 {
+    public int minimumCost(int[] cost) {
+
+        int totalCost = 0;
+        Arrays.sort(cost);
+        int count = 0;
+
+        for (int i = cost.length - 1; i >= 0; i--) {
+            if (count < 2) {
+                totalCost += cost[i];
+                count++;
+            } else {
+                count = 0;
+            }
+        }
+
+        return totalCost;
+    }
+}
+
 public class MinimumCostMain {
+
+    public static void main(String[] args) {
+        Solution03 S03 = new Solution03();
+
+        System.out.println(S03.minimumCost(new int[]{1,2,3}));
+    }
 }

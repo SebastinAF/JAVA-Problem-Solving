b723953ffa4e09b1d976c6acaec807155f1b1f31
SebastinAF <asntheking098@gmail.com>
2026-07-13 10:03:40 +0530

    Insert Solution Added

--- src/Arrays/InsertMain.java
+++ src/Arrays/InsertMain.java
@@ -1,4 +1,29 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution38 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] value : intervals) {
+            if (value[1] < newInterval[0]) {
+                result.add(value);
+            } else if (value[0] > newInterval[1]) {
+                result.add(newInterval);
+                newInterval = value;
+            } else {
+                newInterval[0] = Math.min(value[0], newInterval[0]);
+                newInterval[1] = Math.max(value[1], newInterval[1]);
+            }
+        }
+        result.add(newInterval);
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
 public class InsertMain {
 }
